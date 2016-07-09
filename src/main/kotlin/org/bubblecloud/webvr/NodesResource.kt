bbb426eb27a4e8f8ec057292034cc0c66597a339
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2016-07-09 13:43:47 +0300

    Added methods for updating and deleting node.

--- src/main/kotlin/org/bubblecloud/webvr/NodesResource.kt
+++ src/main/kotlin/org/bubblecloud/webvr/NodesResource.kt
@@ -1,22 +1,43 @@
 package org.bubblecloud.webvr
 
 import java.util.*
-import javax.ws.rs.GET
-import javax.ws.rs.POST
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import javax.ws.rs.*
+import javax.ws.rs.core.Context
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.UriInfo
 
-@Path("nodes")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Path("nodes")
 class NodesResource {
 
-    @GET fun doGet(): List<Node> {
+    @POST fun createNode(node: Node, @Context uriInfo: UriInfo): Response {
+        val nodeId = 3
+        val builder = uriInfo.absolutePathBuilder
+        builder.path(nodeId.toString())
+        println("Created $node.")
+        return Response.created(builder.build()).build()
+    }
+
+    @Path("{nodeId}")
+    @PUT fun updateNode(@PathParam("nodeId") nodeId: Int, node: Node): Response {
+        println("Updated $node.")
+        return Response.ok().build()
+    }
+
+    @Path("{nodeId}")
+    @DELETE fun deleteNode(@PathParam("nodeId") nodeId: Int): Response {
+        println("Deleted $nodeId.")
+        return Response.ok().build()
+    }
+
+    @Path("{nodeId}")
+    @GET fun getNode(@PathParam("nodeId") nodeId: Int): Node {
+        return Node(nodeId)
+    }
+
+    @GET fun getNodes(): List<Node> {
         return Collections.singletonList(Node(1))
     }
-
-    @POST fun doPost(nodes: List<Node>) {
-        println(nodes)
-    }
-
 }
--- src/test/kotlin/org/bubblecloud/webvr/NodesResourceTest.kt
+++ src/test/kotlin/org/bubblecloud/webvr/NodesResourceTest.kt
@@ -9,9 +9,9 @@ import org.junit.Test
 
 import org.junit.Assert.assertEquals
 import javax.ws.rs.client.Entity
-import javax.ws.rs.core.GenericEntity
 import javax.ws.rs.core.GenericType
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 class NodesResourceTest {
 
@@ -36,8 +36,27 @@ class NodesResourceTest {
         assertEquals(1, node.id.toLong())
     }
 
-    @Test fun testPostNodes() {
-        target!!.path("nodes").request().post(Entity.entity(listOf(Node(1)), MediaType.APPLICATION_JSON))
+    @Test fun testGetNode() {
+        val node = target!!.path("nodes/2").request().get(object : GenericType<Node>() {
+
+        })
+        println(node)
+        assertEquals(2, node.id.toLong())
+    }
+
+    @Test fun testCreateNode() {
+        val response: Response = target!!.path("nodes").request().post(Entity.entity(Node(1), MediaType.APPLICATION_JSON))
+        assertEquals(201, response.status)
+        assertEquals(server!!.url + "nodes/3", response.location.toString())
+
+    }
+
+    @Test fun testUpdateNode() {
+        target!!.path("nodes/4").request().put(Entity.entity(Node(4), MediaType.APPLICATION_JSON))
+    }
+
+    @Test fun testDeleteNode() {
+        target!!.path("nodes/5").request().delete()
     }
 
 }
