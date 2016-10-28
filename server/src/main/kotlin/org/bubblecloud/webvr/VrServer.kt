ec8f3d745099a1616238254e338e9e809af35672
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2016-10-28 14:49:39 +0300

    Fixing rest service and static content to work in the same time.

--- server/src/main/kotlin/org/bubblecloud/webvr/VrServer.kt
+++ server/src/main/kotlin/org/bubblecloud/webvr/VrServer.kt
@@ -10,21 +10,17 @@ import org.glassfish.grizzly.websockets.WebSocketAddOn
 import org.glassfish.grizzly.websockets.WebSocketEngine
 import org.bubblecloud.webvr.grizzly.GrizzlyHttpContainer
 import org.glassfish.grizzly.http.server.StaticHttpHandler
-import org.glassfish.jersey.grizzly2.httpserver.internal.LocalizationMessages
 import org.glassfish.jersey.process.JerseyProcessingUncaughtExceptionHandler
 import org.glassfish.jersey.server.ResourceConfig
 import org.glassfish.jersey.server.spi.Container
-import java.io.IOException
 import java.net.URI
-import javax.ws.rs.ProcessingException
 
 import javax.ws.rs.core.UriBuilder
 
 class VrServer(val url: String = "http://localhost:8080/") {
     private val log = logger()
 
-    val config = ResourceConfig(NodeRestService::class.java)
-    val server: HttpServer = createHttpServer( UriBuilder.fromUri(url + "api").build(), GrizzlyHttpContainer(config), false, null, false)
+    val server: HttpServer = createHttpServer( UriBuilder.fromUri(url).build(), false, null)
 
     init {
         System.setProperty("java.util.logging.SimpleFormatter.format","%1\$tT [%4\$s] %5\$s %6\$s %n")
@@ -32,13 +28,6 @@ class VrServer(val url: String = "http://localhost:8080/") {
 
     fun startup(): Unit {
         log.info("VR server startup...")
-
-        server.serverConfiguration.addHttpHandler(StaticHttpHandler("out/resources/main/webapp"))
-        server.getListener("grizzly").registerAddOn(WebSocketAddOn())
-        val wsListener = WebSocketListener()
-        WebSocketEngine.getEngine().register("", "/ws", wsListener)
-
-        server.getListener("grizzly").getFileCache().setEnabled(false);
         server.start()
         log.info("VR server startup.")
     }
@@ -50,10 +39,8 @@ class VrServer(val url: String = "http://localhost:8080/") {
     }
 
     fun createHttpServer(uri: URI,
-                         handler: GrizzlyHttpContainer?,
                          secure: Boolean,
-                         sslEngineConfigurator: SSLEngineConfigurator?,
-                         start: Boolean): HttpServer {
+                         sslEngineConfigurator: SSLEngineConfigurator?): HttpServer {
 
         val host = if (uri.host == null) NetworkListener.DEFAULT_NETWORK_HOST else uri.host
         val port = if (uri.port == -1)
@@ -63,7 +50,9 @@ class VrServer(val url: String = "http://localhost:8080/") {
 
         val listener = NetworkListener("grizzly", host, port)
 
-        listener.transport.workerThreadPoolConfig.threadFactory = ThreadFactoryBuilder().setNameFormat("grizzly-http-server-%d").setUncaughtExceptionHandler(JerseyProcessingUncaughtExceptionHandler()).build()
+        listener.transport.workerThreadPoolConfig.threadFactory = ThreadFactoryBuilder().setNameFormat(
+                "grizzly-http-server-%d").setUncaughtExceptionHandler(
+                JerseyProcessingUncaughtExceptionHandler()).build()
 
         listener.isSecure = secure
         if (sslEngineConfigurator != null) {
@@ -73,28 +62,25 @@ class VrServer(val url: String = "http://localhost:8080/") {
         val server = HttpServer()
         server.addListener(listener)
 
-        // Map the path to the processor.
         val config = server.serverConfiguration
-        if (handler != null) {
-            val path = uri.path.replace("/{2,}".toRegex(), "/")
 
-            val contextPath = if (path.endsWith("/")) path.substring(0, path.length - 1) else path
-            config.addHttpHandler(handler, HttpHandlerRegistration.bulder().contextPath(contextPath).build())
-        }
+        // Add static HTTP handler.
+        config.addHttpHandler(StaticHttpHandler("out/resources/main/webapp"))
+
+        // Add Jersey REST HTTP handler.
+        val restHandler = GrizzlyHttpContainer(ResourceConfig(NodeRestService::class.java))
+        config.addHttpHandler(restHandler, HttpHandlerRegistration.builder().contextPath("/api").build())
+
+
+        // Add web socket support.
+        server.getListener("grizzly").registerAddOn(WebSocketAddOn())
+        val wsListener = WebSocketListener()
+        WebSocketEngine.getEngine().register("", "/ws", wsListener)
+
+        server.getListener("grizzly").getFileCache().setEnabled(false);
 
         config.isPassTraceRequest = true
         config.defaultQueryEncoding = org.glassfish.grizzly.utils.Charsets.UTF8_CHARSET
-
-        if (start) {
-            try {
-                // Start the server.
-                server.start()
-            } catch (ex: IOException) {
-                server.shutdownNow()
-                throw ProcessingException(LocalizationMessages.FAILED_TO_START_SERVER(ex.message), ex)
-            }
-
-        }
 
         return server
     }
