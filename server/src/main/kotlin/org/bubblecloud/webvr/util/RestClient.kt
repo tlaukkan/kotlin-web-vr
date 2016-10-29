96893aa84aa24f1ef1b91b492439bc4d1c52e9df
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2016-10-29 09:19:13 +0300

    Implemented Kotlin rest client wrapper to server side.

---
+++ server/src/main/kotlin/org/bubblecloud/webvr/util/RestClient.kt
@@ -0,0 +1,45 @@
+package org.bubblecloud.webvr.util
+
+import org.bubblecloud.webvr.model.Node
+import java.io.IOException
+import java.util.*
+import javax.ws.rs.client.ClientBuilder
+import javax.ws.rs.client.Entity
+import javax.ws.rs.core.GenericType
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+
+/**
+ * Created by tlaukkan on 10/29/2016.
+ */
+class RestClient<T>(url: String, val path: String, val valueObjectClass: Class<T>) {
+
+    val client = ClientBuilder.newClient()
+    val target = client.target(url)
+
+    fun get(): List<T> {
+        return target.path(path).request().get(object : GenericType<List<T>>(){
+        })
+    }
+
+    fun get(id: UUID): Node {
+        return target.path("$path/$id").request().get(object : GenericType<Node>() {} )
+    }
+
+    fun post(value: T): UUID {
+        val response = target.path(path).request().post(Entity.entity(value, MediaType.APPLICATION_JSON))
+        if (response.status!=Response.Status.CREATED.statusCode) {
+            throw IOException("Rest put failed with status code: ${response.status}.")
+        }
+        return response.readEntity(UUID::class.java)
+    }
+
+    fun put(id: UUID, value: T) {
+        target!!.path("$path/$id").request().put(Entity.entity(value, MediaType.APPLICATION_JSON))
+    }
+
+    fun delete(id: UUID) {
+        target!!.path("$path/$id").request().delete()
+    }
+
+}
--- server/src/test/kotlin/org/bubblecloud/webvr/NodesResourceTest.kt
+++ server/src/test/kotlin/org/bubblecloud/webvr/NodesResourceTest.kt
@@ -1,8 +1,7 @@
 package org.bubblecloud.webvr
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.bubblecloud.webvr.model.Envelope
 import org.bubblecloud.webvr.model.Node
+import org.bubblecloud.webvr.util.RestClient
 import javax.ws.rs.client.ClientBuilder
 import javax.ws.rs.client.WebTarget
 
@@ -12,25 +11,19 @@ import org.junit.Test
 
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
-import java.io.FileInputStream
-import java.net.URI
 import java.util.*
 import java.util.logging.LogManager
-import javax.ws.rs.client.Entity
-import javax.ws.rs.core.GenericType
-import javax.ws.rs.core.MediaType
-import javax.ws.rs.core.Response
 
 class NodesResourceTest {
 
-    private var server: VrServer? = null
+    private var server: VrServer = VrServer()
     private var target: WebTarget? = null
+    private var client: RestClient<Node> = RestClient(server.url + "api", "nodes", Node::class.java)
 
     @Before fun setUp() {
         LogManager.getLogManager().readConfiguration(this.javaClass.getResourceAsStream("/logging.properties"))
 
-        server = VrServer()
-        server!!.startup()
+        server.startup()
 
         val c = ClientBuilder.newClient()
         target = c.target(server!!.url + "api")
@@ -41,31 +34,21 @@ class NodesResourceTest {
     }
 
     @Test fun testNodeResources() {
+        assertEquals(0, client.get().size)
 
-        assertEquals(0, target!!.path("nodes").request().get(object : GenericType<List<Node>>() {
-        }).size)
+        val nodeId: UUID = client.post(Node())
 
-        val response: Response = target!!.path("nodes").request().post(Entity.entity(Node(), MediaType.APPLICATION_JSON))
-        assertEquals(response.status, Response.Status.CREATED.statusCode)
-        val nodeUri: URI = response.location
-        val nodeId: UUID = response.readEntity(UUID::class.java)
-        assertNotNull(nodeUri)
-
-        val node = target!!.path("nodes/$nodeId").request().get(object : GenericType<Node>() {
-        })
+        val node = client.get(nodeId)
 
         assertNotNull(node)
 
-        assertEquals(1, target!!.path("nodes").request().get(object : GenericType<List<Node>>() {
-        }).size)
+        assertEquals(1, client.get().size)
 
-        target!!.path("nodes/$nodeId").request().put(Entity.entity(node, MediaType.APPLICATION_JSON))
+        client.put(nodeId, node)
 
-        target!!.path("nodes/$nodeId").request().delete()
+        client.delete(nodeId)
 
-        assertEquals(0, target!!.path("nodes").request().get(object : GenericType<List<Node>>() {
-        }).size)
-
+        assertEquals(0, client.get().size)
     }
 
 }
