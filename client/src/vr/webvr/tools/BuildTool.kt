228297cf517a10f79e03e5a4a778972954842ebf
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2017-01-15 18:28:30 +0200

    Implementing build tool.

--- client/src/vr/webvr/tools/BuildTool.kt
+++ client/src/vr/webvr/tools/BuildTool.kt
@@ -3,7 +3,11 @@ package vr.webvr.tools
 import lib.threejs.Quaternion
 import lib.threejs.Vector3
 import vr.CLIENT
+import vr.network.model.Node
+import vr.util.dynamicCast
+import vr.util.fromJson
 import vr.util.getDeltaQuaternion
+import vr.util.toJson
 import vr.webvr.devices.InputButton
 import vr.webvr.devices.InputDevice
 
@@ -39,12 +43,6 @@ class BuildTool(inputDevice: InputDevice) : Tool("Build", inputDevice) {
             gripped = false
         }
 
-        if (button == InputButton.TRIGGER) {
-            if (inputDevice.selectNodes() == null) {
-                inputDevice.unselectNodes()
-            }
-        }
-
         if (!gripped) {
             if (button == InputButton.UP) {
                 translate(Vector3(0.0, 0.0, -1.0))
@@ -61,6 +59,12 @@ class BuildTool(inputDevice: InputDevice) : Tool("Build", inputDevice) {
             if (button == InputButton.RIGHT) {
                 translate(Vector3(1.0, 0.0, 0.0))
             }
+
+            if (button == InputButton.TRIGGER) {
+                if (inputDevice.selectNodes() == null) {
+                    inputDevice.unselectNodes()
+                }
+            }
         } else {
             if (button == InputButton.UP) {
                 println("up")
@@ -70,6 +74,30 @@ class BuildTool(inputDevice: InputDevice) : Tool("Build", inputDevice) {
             if (button == InputButton.DOWN) {
                 println("down")
                 translate(Vector3(0.0, -1.0,0.0))
+            }
+
+            if (button == InputButton.TRIGGER) {
+                if (inputDevice.selectNodes() == null) {
+                    inputDevice.unselectNodes()
+                } else {
+
+                    val nodeUrl = inputDevice.selectedNodeUrls.last()
+                    val node = CLIENT!!.vrController.nodes[nodeUrl] ?: return
+                    val nodeType = CLIENT!!.vrController.nodeTypes[nodeUrl] ?: return
+                    val clone : Node = dynamicCast(fromJson(toJson(node)))
+                    clone.url = "${CLIENT!!.vrController.linkedServerCellUrl}/00000000-0000-0000-0000-000000000000"
+
+                    val direction = Vector3(0.0, 0.0, 1.0)
+                    val inputDeviceOrientation = Quaternion(0.0, 0.0, 0.0, 1.0)
+                    inputDevice.entity.getWorldQuaternion(inputDeviceOrientation)
+                    direction.applyQuaternion(inputDeviceOrientation)
+
+                    var obj = CLIENT!!.vrController.scene.getObjectByName(node.url) ?: return
+                    val objectOrientation = Quaternion(0.0, 0.0, 0.0, 1.0)
+                    obj.getWorldQuaternion(objectOrientation)
+
+                    translate(direction, objectOrientation, clone, nodeType)
+                }
             }
         }
     }
@@ -95,42 +123,46 @@ class BuildTool(inputDevice: InputDevice) : Tool("Build", inputDevice) {
             val objectOrientation = Quaternion(0.0, 0.0, 0.0, 1.0)
             obj.getWorldQuaternion(objectOrientation)
 
-            val xAxis = Vector3(1.0, 0.0, 0.0)
-            val yAxis = Vector3(0.0, 1.0, 0.0)
-            val zAxis = Vector3(0.0, 0.0, 1.0)
-
-            xAxis.applyQuaternion(objectOrientation)
-            yAxis.applyQuaternion(objectOrientation)
-            zAxis.applyQuaternion(objectOrientation)
-
-            val xDirection = xAxis.dot(direction)
-            val yDirection = yAxis.dot(direction)
-            val zDirection = zAxis.dot(direction)
-
-            val finalDirection: Vector3
-            if (Math.abs(xDirection) >= Math.abs(yDirection) && Math.abs(xDirection) >= Math.abs(zDirection)) {
-                finalDirection = xAxis.multiplyScalar(sign(xDirection))
-            } else if (Math.abs(yDirection) >= Math.abs(zDirection)) {
-                finalDirection = yAxis.multiplyScalar(sign(yDirection))
-            } else {
-                finalDirection = zAxis.multiplyScalar(sign(zDirection))
-            }
-
-            val x = node.position.x
-            val y = node.position.y
-            val z = node.position.z
-
-            node.position.x += finalDirection.x * node.scale.x
-            node.position.y += finalDirection.y * node.scale.y
-            node.position.z += finalDirection.z * node.scale.z
-
-            CLIENT!!.vrController.networkClient!!.send(node, nodeType)
-
-            node.position.x = x
-            node.position.y = y
-            node.position.z = z
+            translate(direction, objectOrientation, node, nodeType)
         }
 
+    }
+
+    private fun translate(direction: Vector3, objectOrientation: Quaternion, node: Node, nodeType: String) {
+        val xAxis = Vector3(1.0, 0.0, 0.0)
+        val yAxis = Vector3(0.0, 1.0, 0.0)
+        val zAxis = Vector3(0.0, 0.0, 1.0)
+
+        xAxis.applyQuaternion(objectOrientation)
+        yAxis.applyQuaternion(objectOrientation)
+        zAxis.applyQuaternion(objectOrientation)
+
+        val xDirection = xAxis.dot(direction)
+        val yDirection = yAxis.dot(direction)
+        val zDirection = zAxis.dot(direction)
+
+        val finalDirection: Vector3
+        if (Math.abs(xDirection) >= Math.abs(yDirection) && Math.abs(xDirection) >= Math.abs(zDirection)) {
+            finalDirection = xAxis.multiplyScalar(sign(xDirection))
+        } else if (Math.abs(yDirection) >= Math.abs(zDirection)) {
+            finalDirection = yAxis.multiplyScalar(sign(yDirection))
+        } else {
+            finalDirection = zAxis.multiplyScalar(sign(zDirection))
+        }
+
+        val x = node.position.x
+        val y = node.position.y
+        val z = node.position.z
+
+        node.position.x += finalDirection.x * node.scale.x
+        node.position.y += finalDirection.y * node.scale.y
+        node.position.z += finalDirection.z * node.scale.z
+
+        CLIENT!!.vrController.networkClient!!.send(node, nodeType)
+
+        node.position.x = x
+        node.position.y = y
+        node.position.z = z
     }
 
     fun sign(value: Double) : Double {
